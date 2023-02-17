61961142fe5d28c51bc60af6f668fb626dcd92b0
Shivam Dixit <sdstormshadow@gmail.com>
2023-02-18 00:11:45 +0530

    Improved Documentation in Problem1

--- Java/Problem1.java
+++ Java/Problem1.java
@@ -1,9 +1,11 @@
 // hello+your name Program
 import java.util.Scanner;
+//Every time Use Scanner to get input 
+
 class Problem1{
 	public static void main(String[] args) {
 		String start="hello";
-		Scanner sc = new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);//Here we create an object sc
 		System.out.print("Enter your name: ");
 		String name = sc.nextLine();
 		System.out.println(start+"\n"+name);
