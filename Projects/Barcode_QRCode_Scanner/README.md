e03a21fba3299ea4749690452ede2012880b7b7a
Shivam Dixit <sdstormshadow@gmail.com>
2023-02-20 14:53:11 +0530

    QR_Code_Generator and Some Readme modified

---
+++ Projects/Barcode_QRCode_Scanner/README.md
@@ -0,0 +1,3 @@
+Some packages required to work this properly
+pip intall opencv-python
+pip install pyzbar
--- Projects/QR_Code_Generator/qr_code_gen.py
+++ Projects/QR_Code_Generator/qr_code_gen.py
@@ -1,5 +1,11 @@
+import pyttsx3
 import qrcode
-import image   
+import image
+engine = pyttsx3.init()
+newVoiceRate = 160 # specify speak speed
+engine.setProperty('rate',newVoiceRate) # setting up for speak speed
+voices = engine.getProperty('voices')
+engine.setProperty('voice',voices[3].id) # 3 for better english
 qr = qrcode.QRCode (
         version = 15, # To specify pixel
         box_size = 10, # To specify QR Code size
@@ -13,11 +19,13 @@ def custom(s): # function to save file according user specified url
     if s[-1]=='': # To check whether url has subfolder
         s=s[-2]  # assign last second string
         if '.' in s: # We don't require extension So check . 
-            s=s.split('.')[0] # split file by '.' and assign name not extension
+            s=s.split('.')[-2] # split file by '.' and assign name not extension
     else:        
         s=s[-1] # assign direct last name.extension
         if '.' in s: # To '.' extenstion
-            s=s.split('.')[0] # split file by '.' and assign name not extension
-    print("Your QR Code has been generated in same file directory.")
+            s=s.split('.')[-2] # split file by '.' and assign name not extension
     img.save(s+'.png') # saving file in png format
+    engine.say("Your file has been created in current directory")
+    engine.runAndWait() # Waiting time to speak
+    engine.stop() # Stopping the engine
 custom(data.split('/')) # calling where we pass split list by '/'
